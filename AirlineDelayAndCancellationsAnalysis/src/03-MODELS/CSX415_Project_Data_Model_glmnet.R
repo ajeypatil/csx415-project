31c753d56e4c0e3b0c996281dfe693a2199c02a2
ajey <ajey@ajeys-MacBook-Pro-2.local>
2018-05-12 14:59:24 -0700

    update dir

--- AirlineDelayAndCancellationsAnalysis/src/03-MODELS/CSX415_Project_Data_Model_glmnet.R
+++ AirlineDelayAndCancellationsAnalysis/src/03-MODELS/CSX415_Project_Data_Model_glmnet.R
@@ -1,13 +1,14 @@
 Tr_x <- as.data.frame(TrainData[,1:(ncol(TrainData)-1)])
 Tr_x <- model.matrix(~.,data=Tr_x)
-Tr_y <- TrainData[,ncol(TrainData)]
+Tr_y <- as.matrix(TrainData[,ncol(TrainData)])
 glm_net.model <- cv.glmnet(Tr_x, Tr_y)
 
 glm_net.model
 
 Tst_x <- as.data.frame(TestData[,1:(ncol(TrainData)-1)])
 Tst_x <- model.matrix(~.,data=Tst_x)
-Tst_y <- TrainData[,ncol(TrainData)]
+Tst_y <- as.matrix(TrainData[,ncol(TrainData)])
 predictions <- predict(glm_net.model,Tst_x)
 
-glm_net_error <- rmse(TestData$AIRLINE_DELAY,predictions)
+glm_net_error <- rmse(TestData$AIRLINE_DELAY, predictions)
+
