4ca8a50273b915e710a5a2032c1f78dda141e41f
ajey <ajey@ajeys-MacBook-Pro-2.local>
2018-05-26 13:07:31 -0700

    minor

--- AirlineDelayAndCancellationsAnalysis/src/03-MODELS/CSX415_Project_Data_Model_Naive.R
+++ AirlineDelayAndCancellationsAnalysis/src/03-MODELS/CSX415_Project_Data_Model_Naive.R
@@ -5,3 +5,4 @@ nb.model = naive_bayes(TrainData$DelayedOrCancelled ~ .,data=TrainData)
 #saveRDS(nb.model,file="lib/models/flights_naivebayes_model.rds")
 
 nb_predictions <- predict(nb.model,TestData)
+nb_pred_prob <- predict(nb.model, TestData, type="prob")
