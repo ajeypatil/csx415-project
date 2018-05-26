d29a3a257609c07892606a4f29ca0c3aa65da098
ajey <ajey@ajeys-MacBook-Pro-2.local>
2018-05-26 08:53:57 -0700

    minor

--- AirlineDelayAndCancellationsAnalysis/deploy/app/predictFlightDelayCancellation.R
+++ AirlineDelayAndCancellationsAnalysis/deploy/app/predictFlightDelayCancellation.R
@@ -21,11 +21,20 @@ invisible(library(flights))
 
 airline <- opt_get("airline",required=TRUE,description="AIRLINE IATA_CODE")
 origin_airport <- opt_get("origin",required=TRUE,description="ORIGIN AIRPORT IATA_CODE")
-destination_airport <- opt_get("destination",required=TRUE,description="ORIGIN AIRPORT IATA_CODE")
+destination_airport <- opt_get("destination",required=TRUE,description="DESTINATION AIRPORT IATA_CODE")
 month <- opt_get("month",required=TRUE,description="Month 1-12") %>% as.factor()
-day_of_week <- opt_get("day_of_week",required=TRUE,description="Day Of Week") %>% as.factor()
-time_of_day <- opt_get("time",required=TRUE,description="Time 0000-2359") %>% as.numeric()
+day_of_week <- opt_get("day_of_week",required=TRUE,description="Day Of Week") 
+time_of_day <- opt_get("time",required=TRUE,description="Time 0000-2359") 
 #opt_help(name = c("help", "?"), opts = commandArgs())
+
+month <- as.factor(month)
+day_of_week <- as.factor(day_of_week)
+time_of_day <- as.numeric(time_of_day)
+
+if ((time_of_day < 0) | ((time_of_day %% 100) > 59) | ((time_of_day %/% 100) > 23) ) {
+    print("Invalid time entry")
+    exit()
+}
 
 print(paste("Flight with: ",airline," Origin: ",origin_airport," Dest: ",destination_airport," Month: ",month," Day Of week: ",day_of_week," Time of day: ",time_of_day))
 print(" Delay[>60min] Or Cancellation Prediction: ")
